1a40f61442f490abf846668bd5811c7ccdffb505
davidka7 <david.perekopskiy@go.shoreline.edu>
2020-07-01 13:08:00 -0700

    Updates

---
+++ precache-manifest.dcc63d50ea8f4fd4119d6add0758f261.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b7be978e246900e99fe5459f3545702",
+    "url": "/samuel.world/index.html"
+  },
+  {
+    "revision": "1564227f83fc75080ad9",
+    "url": "/samuel.world/static/css/main.dc564d04.chunk.css"
+  },
+  {
+    "revision": "5f5fbd0f5ed98f42c8fa",
+    "url": "/samuel.world/static/js/2.1ea635c5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/samuel.world/static/js/2.1ea635c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1564227f83fc75080ad9",
+    "url": "/samuel.world/static/js/main.b5a041a6.chunk.js"
+  },
+  {
+    "revision": "4110b268a3406beb7b95",
+    "url": "/samuel.world/static/js/runtime-main.509c0e43.js"
+  },
+  {
+    "revision": "b7e985fda7668be398488469e52924df",
+    "url": "/samuel.world/static/media/lol.b7e985fd.jpg"
+  }
+]);
